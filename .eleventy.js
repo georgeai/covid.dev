a57384e29909e8037eb7ddb20ac157bf6fa7405d
George Shih <george@chez.ai>
2020-04-21 21:16:16 -0400

    added markdown-it for linkify-it support; added .eleventy.js to enable markdown-it support

---
+++ .eleventy.js
@@ -0,0 +1,91 @@
+// const pluginSyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
+// const htmlmin = require('html-minifier')
+// const xmlPlugin = require('eleventy-xml-plugin')
+// const nbspFilter = require('eleventy-nbsp-filter')
+
+module.exports = function(eleventyConfig) {
+  /* FILTERS AND PLUGINS */
+  // eleventyConfig.addPlugin(xmlPlugin)
+  // eleventyConfig.addPlugin(pluginSyntaxHighlight)
+  // eleventyConfig.addFilter('nbsp', nbspFilter(2, 12))
+
+  /* COLLECTIONS */
+  /*
+  eleventyConfig.addCollection('blogposts', (collection) => {
+    return collection
+      .getFilteredByTag('post')
+      .reverse()
+      .slice(0, 9000)
+  })
+
+  eleventyConfig.addCollection('archive', (collection) => {
+    return collection.getFilteredByTag('post').reverse()
+  })
+
+  eleventyConfig.addCollection('all', function(collection) {
+    return collection.getAll()
+  })
+  */
+  /* SHORT CODES */
+  /*
+  eleventyConfig.addShortcode('actionLink', (link) => {
+  return `<em class="action--link">See this example <a aria-label="launch this code snippet" href=${link} rel="noopener noreferrer" target="_blank">in action</a></em>`
+  })
+  */
+
+  /* LIQUID CONFIG */
+  // eleventyConfig.setLiquidOptions({ dynamicPartials: true })
+  // eleventyConfig.addLayoutAlias('home', 'layouts/home.liquid')
+  // eleventyConfig.addLayoutAlias('default', 'layouts/default.liquid')
+
+  // eleventyConfig.setUseGitIgnore(false)
+
+  /* MARKDOWN */
+  const markdownIt = require('markdown-it')
+  // const markdownItEmoji = require('markdown-it-emoji')
+  // const markdownItAnchor = require('markdown-it-anchor')
+  const markdownItOptions = {
+    html: true,
+    breaks: true,
+    linkify: true
+  }
+  // const markdownItAnchorOptions = {
+  //   permalink: true,
+  //   permalinkSymbol: '#',
+  //   permalinkBefore: true,
+  //   permalinkClass: 'heading--anchor',
+  //   level: [2, 3, 4]
+  // }
+  const markdownLib = markdownIt(markdownItOptions)
+  //   .use(markdownItAnchor, markdownItAnchorOptions)
+  //   .use(markdownItEmoji)
+  eleventyConfig.setLibrary('md', markdownLib)
+
+  /* COPY */
+  // eleventyConfig.addPassthroughCopy('assets')
+  // eleventyConfig.addPassthroughCopy('robots.txt')
+
+  /* HTML */
+  /*
+  eleventyConfig.addTransform('htmlmin', (content, outputPath) => {
+    if (outputPath.endsWith('.html')) {
+      const minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      })
+
+      return minified
+    }
+
+    return content
+  })
+
+  // return base config
+  return {
+    input: './',
+    output: './_site',
+    passthroughFileCopy: true
+  }
+  */
+}
